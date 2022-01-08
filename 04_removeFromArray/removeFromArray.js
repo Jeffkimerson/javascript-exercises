1824b0c517b3b19776a3a12fb43b617b80b42845
Jeff Kim <jeffersontaekim@gmail.com>
2022-01-08 04:54:01 -0800

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    for(let i = 0; i < remove.length; i++) {
+        if(arr.indexOf(remove[i]) != -1) {
+            arr.splice(arr.indexOf(remove[i]), 1);
+        }
+    }
+    return arr;
 
 };
 
