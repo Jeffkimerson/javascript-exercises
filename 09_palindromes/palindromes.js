23a60c1dcf1448b4beb394b74ff0e21c7148f06a
Jeffkimerson <85892155+Jeffkimerson@users.noreply.github.com>
2022-01-10 09:16:12 -0800

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    let abc = /[a-z]/;
+    let phrase = '';
+    let revPhrase = '';
 
+    for (let i = 0; i < str.length; i++) {
+        if(abc.test(str[i])) {
+            phrase += str[i];
+            revPhrase = str[i] + revPhrase;
+        }
+    }
+    if (phrase == revPhrase) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
