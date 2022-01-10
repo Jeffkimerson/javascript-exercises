2f3dc6758a7e4e3f13f3b5e204cc13035755aed2
Jeffkimerson <85892155+Jeffkimerson@users.noreply.github.com>
2022-01-10 08:23:17 -0800

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    if (num2 < num1) {
+        [num1, num2] = [num2, num1];
+    }
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
