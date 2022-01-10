b4e6e9277404fb1ea556b22a6480fc58b7860eb3
Jeffkimerson <85892155+Jeffkimerson@users.noreply.github.com>
2022-01-10 09:02:08 -0800

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (let i = num; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
