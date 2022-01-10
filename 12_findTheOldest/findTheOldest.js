c9db9211e6ba2427040c0874fdaf3573eed2e33a
Jeffkimerson <85892155+Jeffkimerson@users.noreply.github.com>
2022-01-10 11:49:20 -0800

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldest = 0;
+    let index;
+    let year = new Date().getFullYear();
+    console.log(arr);
+    for (let i = 0; i < arr.length; i++) {
+       let age;
+        if (!arr[i].hasOwnProperty('yearOfDeath')) {
+            age = year - arr[i].yearOfBirth;
+        }
+        else {
+        age = arr[i].yearOfDeath - arr[i].yearOfBirth; 
+        }
+        if (age > oldest) {
+            oldest = age;
+            index = i;
+        }
+        
+    }
+    return arr[index];
 
 };
 
